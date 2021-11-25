4b8fa3a7df15e7deba7459c0f8a24afac69e0029
aldairDiazLopez97 <aldair14101997@gmail.com>
2021-11-25 21:25:22 +0100

    Otro commit

--- app/src/main/java/com/aplicaciones/pedidosapp/Banner.kt
+++ app/src/main/java/com/aplicaciones/pedidosapp/Banner.kt
@@ -6,20 +6,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.MenuItem
-import android.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.aplicaciones.pedidosapp.databinding.ActivityBannerBinding
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Banner : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var drawer: DrawerLayout
     private lateinit var toggle: ActionBarDrawerToggle
 
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityBannerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_banner)
+        binding = ActivityBannerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // Initialize Firebase Auth
+        auth = Firebase.auth
 
 
         val toolbar: androidx.appcompat.widget.Toolbar= findViewById(R.id.toolbar_main)
@@ -43,9 +52,15 @@ class Banner : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListe
             R.id.nav_item_platillos -> abrirPlatillos()
             R.id.nav_item_carrito -> abrirCarrito()
             R.id.nav_item_perfil -> abrirPerfil()
+            R.id.nav_item_logout -> signOut()
         }
         drawer.closeDrawer(GravityCompat.START)
         return true
+    }
+    private fun signOut() {
+        Firebase.auth.signOut()
+        val intent = Intent(this, SignInActivity::class.java)
+        startActivity(intent)
     }
 
     private fun abrirPerfil() {

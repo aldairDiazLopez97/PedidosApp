5df00ef6fc12a7d620b2b568fe2c393526080b7d
aldairDiazLopez97 <aldair14101997@gmail.com>
2021-11-25 17:47:15 +0100

    Otro commit

---
+++ app/src/main/java/com/aplicaciones/pedidosapp/CarritoActivity.kt
@@ -0,0 +1,11 @@
+package com.aplicaciones.pedidosapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CarritoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_carrito)
+    }
+}

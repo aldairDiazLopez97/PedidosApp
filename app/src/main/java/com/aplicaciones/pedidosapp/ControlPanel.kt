f28c06bbd840886519b858810965f4cee1bfbb07
XCDarkingCX <eucebiocardenasmeza2019@gmail.com>
2021-11-25 00:44:16 -0500

    Panel

---
+++ app/src/main/java/com/aplicaciones/pedidosapp/ControlPanel.kt
@@ -0,0 +1,11 @@
+package com.aplicaciones.pedidosapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ControlPanel : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_control_panel)
+    }
+}

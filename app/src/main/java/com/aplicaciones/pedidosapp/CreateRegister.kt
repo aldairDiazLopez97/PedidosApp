65470146349856db04659a6a3be5091a74607922
aldairDiazLopez97 <aldair14101997@gmail.com>
2021-11-26 03:52:01 +0100

    Otro commit

--- app/src/main/java/com/aplicaciones/pedidosapp/CreateRegister.kt
+++ app/src/main/java/com/aplicaciones/pedidosapp/CreateRegister.kt
@@ -19,6 +19,7 @@ class CreateRegister : AppCompatActivity() {
     private val SELECT_ACTIVITY = 50
     private var imageUri: Uri? = null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -26,45 +27,47 @@ class CreateRegister : AppCompatActivity() {
         binding = ActivityCreateRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val listTy = resources.getStringArray(R.array.opciones)
-
         val categoria = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,listTy)
-
-
         binding.Tipo.adapter=categoria
 
         binding.img.setOnClickListener {
             ImageController.selectPhoto(this, SELECT_ACTIVITY)
         }
-
         binding.tvRegister.setOnClickListener{
+
             val nombre= binding.Nombre.text.toString()
             val descripcion= binding.Descripcion.text.toString()
             val precio= binding.Precio.text.toString().toDouble()
-            var tipo = binding.Tipo.adapter.toString()
-
+            val imageUri = ImageController.getImageUri(this, nombre.toLong())
             binding.img.setImageURI(imageUri)
+
+
             binding.Tipo.onItemSelectedListener = object:
                 AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                    tipo = listTy[position].toString()
-                }
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
 
+                    var tipo = binding.Tipo.selectedItem.toString()
+
+                    database=FirebaseDatabase.getInstance().getReference("Platillos")
+                    var Platillos= Plate(nombre,descripcion,precio,tipo,imageUri)
+                    database.child(nombre).setValue(Platillos).addOnSuccessListener {
+                        binding.Nombre.text?.trim()
+                        binding.Descripcion.text?.trim()
+                        binding.Precio.text?.trim()
+                    }
+
+                    Toast.makeText(this@CreateRegister,"Registro Exitoso",Toast.LENGTH_LONG).show()
+                }
                 override fun onNothingSelected(p0: AdapterView<*>?) {
-                    TODO("Not yet implemented")
+                    Toast.makeText(this@CreateRegister,"No se completo el registro",Toast.LENGTH_LONG).show()
+
                 }
-
             }
-            database=FirebaseDatabase.getInstance().getReference("Platillos")
-            val Platillos= Plate(nombre,descripcion,precio,tipo)
-            database.child(nombre).setValue(Platillos).addOnSuccessListener {
-                binding.Nombre.text?.clear()
-                binding.Descripcion.text?.clear()
-                binding.Precio.text?.clear()
-            }
-
-
-
-            /**/
 
         }
     }
